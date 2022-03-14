91b2779e96f992b30508d0e18649b4bd06dda046
Lucas Lobato Botelho <criptoryu@gmail.com>
2022-03-14 23:56:53 +0100

    Learning more DOM Manipulation using,append(),appendChild(),nextElementSibling(),prepend(),before(),after() etc...

--- PART II - BROWSER AND DOM EXERCISES/script.js
+++ PART II - BROWSER AND DOM EXERCISES/script.js
@@ -18,7 +18,23 @@ const previousH2Sibling = nextH2Sibling.previousElementSibling;
 
 // Crea un nuevo elemento
 const nuevoH2 = document.createElement('h2');
-nuevoH2.textContent = "Este es un h2 creado con JavaScript";
+nuevoH2.textContent = "Node.appendChild()";
 
-// Inserta un elemento después del elemento hijo del elemento especificado. (article:nth-of-type(2n))
-const articulo = document.querySelector('article:nth-of-type(2n)').appendChild(nuevoH2);
+
+
+// Inserta un div después del ultimo hijo del padre especificado (article:nth-of-type(2n))
+const nuevoDiv= document.createElement('div');
+const articulo = document.querySelector('article:nth-of-type(2n)').appendChild(nuevoDiv);
+
+//Selecciona el div creado y append el h2 creado más arriba
+const selectorDiv = document.querySelector('article:nth-of-type(2n) div:nth-of-type(4n)');
+const nuevoParrafo = document.createElement('p');
+selectorDiv.append(nuevoH2,nuevoParrafo);
+
+const parrafos = document.querySelectorAll('p');
+parrafos[6].innerHTML = `El método <code>appendChild</code> de la interfaz <code>Node</code>, añade
+un nodo al final de la lista de hijos del padre espeficicado. Si el hijo ya existe en el documento,
+este se mueve de su posición actual a la nueva posición.`;
+
+const articles = document.querySelectorAll('article');
+articles[1].insertAdjacentElement('afterbegin',nuevoDiv)
